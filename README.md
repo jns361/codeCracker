2ae91e51204de20ca21484510505ebd0951c8a26
Mona Lisa <jonas.bauriedl@t-online.de>
2025-07-30 14:11:51 +0200

    Initial commit; First version of CodeCracker
    Added the full C# console game including the README.
    Content of this version can be found in the README.

---
+++ README.md
@@ -0,0 +1,28 @@
+# 🎮CodeCracker
+A simple C# console game where you guess a secret 4-digit code with limited tries – inspired by Wordle.
+
+# ❓How does it work
+First you choose your difficulty. You can choose between Easy, Medium, and Hard. The different difficulties affect your total amount of guesses you have, nothing else.
+
+After you're done with that, a random 4-digit code is generated. You have to guess this code in a certain amount of tries.
+When entering a guess (with the numbers from 0-9), you instantly get feedback inspired by the famous game 'Wordle'.
+You're told how many of the digits in your guess are already correct and which are in the code, but not on their correct position.
+This feedback can look like this:
+
+X digit/s are correct and on the right position!
+X digit/s are correct but on the wrong position!
+
+X represents the amount of digits that fit the sentence.
+
+The game ends when you either guess the exact code or you have no guesses left.
+You can always choose to play again in the end.
+
+# 💡How I got the Idea
+I just wanted to repeat most of the things I've learned until today and asked an AI to give me a small exercise. This small exercise was small in the beginning, but I ended up getting more ideas of what could possibly be added and tried it. This now is the first complete result that I'm comfortable showing publicly.
+
+# 💭My thoughts on the project
+I had a lot of fun coding this. I think regardless of its simplicity it's a fun little game that brings fun, and if it does that only a few minutes a week, that still makes me happy.
+
+# 📝Learning progression
+The project was nice for repeating all of the stuff I've learned until now. I got to practice everything in a fun way and it definetly also taught me something new! 
+In my opinion, this is a solid base I can build on considering I started with C# just a bit more than a week ago.
---
+++ codeCracker/codeCracker/Program.cs
@@ -0,0 +1,188 @@
+﻿namespace codeCracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameLoop();
+        }
+
+        static string keepPlaying = "y";
+
+        static void GameLoop()
+        {
+
+            while (keepPlaying == "y")
+            {
+                Console.Clear();
+                int guessLimit = DifficultyChoice();
+                //Console.Write(guessLimit);                                                                        (Debugline)
+                string secretCode = RandomCode();
+
+                Console.WriteLine("\nThe 4-digit secret code is generated.");
+
+                int guessCount = 0;
+
+                while (true)
+
+                {
+                    guessCount++;
+
+                    string userGuess = GetUserGuess();
+
+                    var (correctPosition, wrongPosition) = EvaluateGuess(userGuess, secretCode);                    //get both variables out of one methodcall by making use of tuples
+
+
+                    Console.WriteLine($"{correctPosition} digit/s are correct and on the right position!");
+                    Console.WriteLine($"{wrongPosition} digit/s are correct but on the wrong position!");
+
+                    //winning, no tries left or keep playing
+                    if (correctPosition == 4)                                                                       //check if 4/4 digits are guessed in the correct position
+                    {
+                        Console.WriteLine("You got the code right!");
+                        Console.WriteLine($"It was {userGuess}.");
+                        break;
+                    }
+
+                    if (correctPosition != 4)
+                    {
+                        if (guessCount == guessLimit)
+                        {
+                            Console.WriteLine("\nNo more tries left.");
+                            Console.WriteLine($"The code was {secretCode}.");
+                            break;
+                        }
+                        else if (guessCount == guessLimit - 1)
+                        {
+                            Console.WriteLine("\nOnly 1 try left!");
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+
+                }
+
+                while (true)
+                {
+                    Console.Write("\nDo you want to play again? \nY or N: ");
+                    keepPlaying = Console.ReadLine().ToLower();
+                    if (keepPlaying != "y" && keepPlaying != "n")
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        static string GetUserGuess()
+        {
+            while (true)
+            {
+                Console.Write("\nGuess: ");
+                string userGuess = Console.ReadLine();
+
+                if (userGuess.Length == 4 && int.TryParse(userGuess, out _))
+                {
+                    return userGuess;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input. Check and enter again!");
+                    continue;
+                }
+            }
+        }
+
+
+        static (int correctPosition, int wrongPosition) EvaluateGuess(string userGuess, string secretCode)
+        {
+            int correctPosition = 0;
+            int wrongPosition = 0;
+
+            char[] secretArray = secretCode.ToCharArray();
+            char[] guessArray = userGuess.ToCharArray();
+
+            for (int i = 0; i < secretCode.Length; i++)
+            {
+                if (userGuess[i] == secretCode[i])
+                {
+                    correctPosition++;
+                    secretArray[i] = '*';
+                    guessArray[i] = '#';
+                }
+            }
+
+            for (int i = 0; i < guessArray.Length; i++)
+            {
+                if (guessArray[i] != '#')
+                {
+                    int index = Array.IndexOf(secretArray, guessArray[i]);
+                    if (index != -1)
+                    {
+                        wrongPosition++;
+                        secretArray[index] = '*';
+                    }
+                }
+            }
+            return (correctPosition, wrongPosition);
+        }
+
+        static Random generator = new Random();
+
+        static string RandomCode()
+        {
+            return generator.Next(1000, 10000).ToString();
+        }
+
+
+        static int DifficultyChoice()
+        {
+            string choice = string.Empty;
+            while (true)
+            {
+                Console.WriteLine("Choose your difficulty:\n1 - Easy -> 15 guesses" +
+                    "\n2 - Medium -> 10 guesses\n3 - Hard -> 6 guesses");
+
+                Console.Write("Choice: ");
+                choice = Console.ReadLine();
+
+                if (choice != "1" && choice != "2" && choice != "3")
+                {
+                    Console.WriteLine("No accepted choice! You may only enter '1', '2' or '3'!\n");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //declaring the amount of guesses to a variable to handle it easier
+            int intChoice = int.Parse(choice);
+            if (intChoice == 1)
+            {
+                int guessLimit = 15;
+                return guessLimit;
+            }
+            else if (intChoice == 2)
+            {
+                int guessLimit = 10;
+                return guessLimit;
+            }
+            else
+            {
+                int guessLimit = 6;
+                return guessLimit;
+            }
+
+        }
+    }
+}
